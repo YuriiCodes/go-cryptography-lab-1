9798ba6bc64b7a3d2a33bc1e281b64b26a20b134
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2023-09-20 21:34:32 +0300

    add form layout

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 
@@ -20,24 +21,33 @@ func main() {
 	eulerPhiInput := widget.NewEntry()
 	resultLabel := widget.NewLabel("") // Create an empty label to display the result.
 
+	form := &widget.Form{
+		Items: []*widget.FormItem{
+			{Text: "Enter N for EulerPhi", Widget: eulerPhiInput},
+			{Text: "Result", Widget: resultLabel},
+			{Widget: widget.NewButton("Calculate", func() {
+				// Retrieve the text from the Entry widget and update the result label.
+				parserEulerPhiInput, err := strconv.Atoi(eulerPhiInput.Text)
+				if err != nil {
+					// write 'invalid input' to the result label:
+					dialog.ShowError(
+						errors.New(fmt.Sprintf("Invalid integer input: \n%s", err)), myWindow)
+				}
+				fmt.Println("EulerPhi input: ", parserEulerPhiInput)
+				fmt.Println("EulerPhi result: ", task1.EulerPhi(parserEulerPhiInput))
+
+				result := "The result is: " + strconv.Itoa(task1.EulerPhi(parserEulerPhiInput))
+				resultLabel.SetText(result)
+			})},
+		},
+		SubmitText: "Calculate",
+		OnSubmit:   nil, // You can add a callback here if needed.
+	}
+
 	tabs := container.NewAppTabs(
 		container.NewTabItem("Task 1",
 			container.NewVBox(
-				widget.NewLabel("Enter N for EulerPhi"),
-				eulerPhiInput,
-				widget.NewButton("Calculate", func() {
-					// Retrieve the text from the Entry widget and update the result label.
-					parserEulerPhiInput, err := strconv.Atoi(eulerPhiInput.Text)
-					if err != nil {
-						dialog.ShowError(err, myWindow)
-					}
-					fmt.Println("EulerPhi input: ", parserEulerPhiInput)
-					fmt.Println("EulerPhi result: ", task1.EulerPhi(parserEulerPhiInput))
-
-					result := "The result is: " + strconv.Itoa(task1.EulerPhi(parserEulerPhiInput))
-					resultLabel.SetText(result)
-				}),
-				resultLabel,
+				form,
 			),
 		),
 	)

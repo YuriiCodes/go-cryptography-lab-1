3373d13650b2f656a88a6ae0280378085c06b52d
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2023-09-20 21:31:32 +0300

    gui init

--- main.go
+++ main.go
@@ -1,21 +1,49 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+
 	"crypto-lab-1/task1"
-	"crypto-lab-1/task2"
-	"crypto-lab-1/task3"
-	"crypto-lab-1/task4"
-	"crypto-lab-1/task5"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	task1.View()
+	myApp := app.New()
+	myWindow := myApp.NewWindow("TabContainer Widget")
+	myWindow.Resize(fyne.NewSize(800, 600))
 
-	task2.View()
+	eulerPhiInput := widget.NewEntry()
+	resultLabel := widget.NewLabel("") // Create an empty label to display the result.
 
-	task3.View()
+	tabs := container.NewAppTabs(
+		container.NewTabItem("Task 1",
+			container.NewVBox(
+				widget.NewLabel("Enter N for EulerPhi"),
+				eulerPhiInput,
+				widget.NewButton("Calculate", func() {
+					// Retrieve the text from the Entry widget and update the result label.
+					parserEulerPhiInput, err := strconv.Atoi(eulerPhiInput.Text)
+					if err != nil {
+						dialog.ShowError(err, myWindow)
+					}
+					fmt.Println("EulerPhi input: ", parserEulerPhiInput)
+					fmt.Println("EulerPhi result: ", task1.EulerPhi(parserEulerPhiInput))
 
-	task4.View()
+					result := "The result is: " + strconv.Itoa(task1.EulerPhi(parserEulerPhiInput))
+					resultLabel.SetText(result)
+				}),
+				resultLabel,
+			),
+		),
+	)
 
-	task5.View()
+	tabs.SetTabLocation(container.TabLocationLeading)
+
+	myWindow.SetContent(tabs)
+	myWindow.ShowAndRun()
 }
